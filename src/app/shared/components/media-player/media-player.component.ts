04cf3714bd2cd09369901a75333e2485c677e547
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-23 14:44:35 -0400

    Layout finish

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
-  styleUrls: ['./media-player.component.css']
+  styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent {
+  mockCover: any = {
+    cover:
+      'https://e-cdn-images.dzcdn.net/images/cover/9b6da786cd3ca8b286a04186b3c9079c/264x264-000000-80-0-0.jpg',
+    album: '?',
+    name: 'Moonlight',
+  };
 
+  ngOnInit() {}
 }
