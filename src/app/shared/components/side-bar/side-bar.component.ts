3faa4e7360551635832acd6b3ce1c5810b52980e
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-23 11:28:05 -0400

    List of options and access user

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -3,8 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
 })
 export class SideBarComponent {
+  mainMenu: { defaultOptions: Array<any>; accessLink: Array<any> } = {
+    defaultOptions: [],
+    accessLink: [],
+  };
 
+  ngOnInit(): void {
+    //* List of options
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Inicio',
+        icon: 'uil-estate',
+        router: ['/'],
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil-search',
+        router: ['/', 'history'],
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil-book-open',
+        router: ['/', 'favorites'],
+      },
+    ];
+
+    //* Access user
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square',
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical',
+      },
+    ];
+  }
 }
