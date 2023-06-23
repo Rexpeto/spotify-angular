23711b78f325c72fc9695d36b011eacaac142d3d
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-23 13:24:14 -0400

    Router auth and index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
-    {
-        path: "",
-        loadChildren: () =>
-            import(`./modules/home/home.module`).then(m => m.HomeModule)
-    }
+  {
+    path: '',
+    loadChildren: () =>
+      import(`./modules/home/home.module`).then((m) => m.HomeModule),
+    component: HomePageComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import(`./modules/auth/auth.module`).then((m) => m.AuthModule),
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
