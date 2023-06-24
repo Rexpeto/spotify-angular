0dcf73ae24dae2438238b8b8198d8b7e23f70f9b
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-24 11:17:03 -0400

    Exports component CardPrayer and SectionGeneric

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,24 @@ import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { CardPrayerComponent } from './components/card-prayer/card-prayer.component';
+import { SectionGenericComponent } from './components/section-generic/section-generic.component';
 
 @NgModule({
-  declarations: [SideBarComponent, MediaPlayerComponent, HeaderUserComponent],
+  declarations: [
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent,
+    CardPrayerComponent,
+    SectionGenericComponent,
+  ],
   imports: [CommonModule],
-  exports: [SideBarComponent, MediaPlayerComponent, HeaderUserComponent],
+  exports: [
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent,
+    CardPrayerComponent,
+    SectionGenericComponent,
+  ],
 })
 export class SharedModule {}
