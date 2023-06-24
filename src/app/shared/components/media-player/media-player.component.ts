85fe83cb072de14e42e1b8e875452d1c93ed5e59
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-24 11:57:32 -0400

    Use interface TrackModel

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
@@ -6,11 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent {
-  mockCover: any = {
+  mockCover: TrackModel = {
+    _id: 1,
     cover:
       'https://e-cdn-images.dzcdn.net/images/cover/9b6da786cd3ca8b286a04186b3c9079c/264x264-000000-80-0-0.jpg',
     album: '?',
     name: 'Moonlight',
+    artist: {
+      name: 'XXXTENTACION',
+      nickname: 'xxxtentacion',
+      nationality: 'US',
+    },
+    url: 'http://localhost:4000/x.mp3',
   };
 
   ngOnInit() {}
