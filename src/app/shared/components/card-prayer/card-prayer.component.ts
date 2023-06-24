b9ffcfed02d62e112e68786e6002cbb60a42a487
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-24 13:05:15 -0400

    Input and model

--- src/app/shared/components/card-prayer/card-prayer.component.ts
+++ src/app/shared/components/card-prayer/card-prayer.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-card-prayer',
   templateUrl: './card-prayer.component.html',
-  styleUrls: ['./card-prayer.component.css']
+  styleUrls: ['./card-prayer.component.css'],
 })
 export class CardPrayerComponent {
-
+  @Input() mode: 'small' | 'big' = 'small';
+  @Input() track!: TrackModel;
 }
