18afd7f632db00373461ae78c230936c77a3c391
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-25 14:32:07 -0400

    Fn changeSort of asc to desc

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -10,9 +10,23 @@ import { TrackModel } from '@core/models/tracks.model';
 export class PlayListBodyComponent {
   tracks: TrackModel[] = [];
 
+  optionSort: { property: string | null; order: string } = {
+    property: null,
+    order: 'asc',
+  };
+
   ngOnInit() {
     const { data }: any = (dataRaw as any).default;
 
     this.tracks = data;
+  }
+
+  changeSort(property: string): void {
+    const { order } = this.optionSort;
+
+    this.optionSort = {
+      property,
+      order: order === 'asc' ? 'desc' : 'asc',
+    };
   }
 }
