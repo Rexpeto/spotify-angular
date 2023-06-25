24c7b8a44891d12decba20f762eaabc0c7798520
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-25 17:47:18 -0400

    Validate form and sendLogin

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
+  formLogin: FormGroup = new FormGroup({});
 
+  ngOnInit() {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ]),
+    });
+  }
+
+  sendLogin(): void {
+    const body = this.formLogin.value;
+
+    console.log(body);
+  }
 }
