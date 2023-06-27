3f009d390909e2fc234bdc982dc335ce35235ece
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-26 23:16:08 -0400

    receiving track

--- src/app/shared/components/card-prayer/card-prayer.component.ts
+++ src/app/shared/components/card-prayer/card-prayer.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
 
 @Component({
   selector: 'app-card-prayer',
@@ -7,6 +8,8 @@ import { TrackModel } from '@core/models/tracks.model';
   styleUrls: ['./card-prayer.component.css'],
 })
 export class CardPrayerComponent {
+  constructor(private _multimediaService: MultimediaService) {}
+
   @Input() mode: 'small' | 'big' = 'small';
   @Input() track: TrackModel = {
     _id: '',
@@ -20,4 +23,8 @@ export class CardPrayerComponent {
     cover: '',
     url: '',
   };
+
+  sendPlay(track: TrackModel) {
+    this._multimediaService.callback.emit(track);
+  }
 }
