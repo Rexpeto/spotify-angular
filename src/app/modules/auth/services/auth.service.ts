12196fb5d0a03af97609d9302d8648e9410d2ef7
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-26 22:25:56 -0400

    Service of auth

---
+++ src/app/modules/auth/services/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor() {}
+
+  sendCredential(email: string, password: string): void {
+    console.log(email, password);
+  }
+}
