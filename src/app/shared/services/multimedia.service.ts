ae359a74234501d937814c25c5ed50a35f22faaf
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-26 23:14:46 -0400

    Callback event

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MultimediaService {
+  callback: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {}
 }
