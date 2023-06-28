6329500fb330c4e43b08dd350ef2bad87b36525d
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-27 20:47:46 -0400

    Http client call api tracks

--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,36 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { TrackModel } from '@core/models/tracks.model';
-import { Observable, of } from 'rxjs';
-import * as dataRaw from '../../../data/track.json';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrackService {
-  dataTracksTrending$: Observable<TrackModel[]> = of([]);
-  dataTracksRandom$: Observable<any> = of([]);
+  private readonly URL = environment.api;
 
-  constructor() {
-    const { data } = dataRaw;
-    this.dataTracksTrending$ = of(data);
+  constructor(private httpClient: HttpClient) {}
 
-    this.dataTracksRandom$ = new Observable((observer) => {
-      const trackExample: TrackModel = {
-        _id: 10,
-        name: 'ejemplo',
-        album: 'ejemplo',
-        cover: 'ejemplo',
-        artist: {
-          name: 'Ejemplo',
-          nickname: 'Ejemplo',
-          nationality: 'Ve',
-        },
-        url: 'http:/local',
-      };
-
-      setTimeout(() => {
-        observer.next([trackExample]);
-      }, 3000);
-    });
+  getAllTracks$(): Observable<any> {
+    return this.httpClient.get(`${this.URL}/tracks`);
   }
 }
