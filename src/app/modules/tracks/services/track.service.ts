403cdfd3f8e3bf54fe06adaf2e3238d2d469515c
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-29 08:42:38 -0400

    Services getAllRandom and getAllTracks

--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -12,6 +13,14 @@ export class TrackService {
   constructor(private httpClient: HttpClient) {}
 
   getAllTracks$(): Observable<any> {
-    return this.httpClient.get(`${this.URL}/tracks`);
+    return this.httpClient
+      .get(`${this.URL}/tracks`)
+      .pipe(map(({ data }: any) => data));
+  }
+
+  getAllRandom$(): Observable<any> {
+    return this.httpClient
+      .get(`${this.URL}/tracks`)
+      .pipe(map(({ data }: any) => data.reverse()));
   }
 }
