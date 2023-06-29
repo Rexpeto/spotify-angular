e7b5031b7c3ebba628ffb26b20bb85cde7a1a1b2
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-29 16:55:16 -0400

    Use canActive in route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SessionGuard } from '@core/guards/session.guard';
 import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
@@ -8,6 +9,7 @@ const routes: Routes = [
     loadChildren: () =>
       import(`./modules/home/home.module`).then((m) => m.HomeModule),
     component: HomePageComponent,
+    canActivate: [SessionGuard],
   },
   {
     path: 'auth',
