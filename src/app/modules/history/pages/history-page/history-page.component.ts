b807c9e120cfab1b9d6b486b90eaaba2daedda35
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-01 17:32:11 -0400

    Use service Search

--- src/app/modules/history/pages/history-page/history-page.component.ts
+++ src/app/modules/history/pages/history-page/history-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { SearchService } from '@modules/history/services/search.service';
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
-  styleUrls: ['./history-page.component.css']
+  styleUrls: ['./history-page.component.css'],
 })
 export class HistoryPageComponent {
+  listResult: TrackModel[] = [];
 
+  constructor(private _searchService: SearchService) {}
+
+  receiveData(term: string): void {
+    this._searchService.searchTrack$(term).subscribe((res) => {
+      this.listResult = res.data;
+    });
+  }
 }
