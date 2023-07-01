ba950b08d9cc1c0c263f5b3afa850ab1aea4ef28
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-01 17:31:31 -0400

    Input list tracks

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import * as dataRaw from '../../../data/track.json';
+import { Component, Input } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
@@ -8,18 +7,14 @@ import { TrackModel } from '@core/models/tracks.model';
   styleUrls: ['./play-list-body.component.css'],
 })
 export class PlayListBodyComponent {
-  tracks: TrackModel[] = [];
+  @Input() tracks: TrackModel[] = [];
 
   optionSort: { property: string | null; order: string } = {
     property: null,
     order: 'asc',
   };
 
-  ngOnInit() {
-    const { data }: any = (dataRaw as any).default;
-
-    this.tracks = data;
-  }
+  ngOnInit() {}
 
   changeSort(property: string): void {
     const { order } = this.optionSort;
