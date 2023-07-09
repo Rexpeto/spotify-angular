572f5d782866b4cbb6333e4f59e1075eb114cb34
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-08 22:38:25 -0400

    Play music

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +8,20 @@ import { EventEmitter, Injectable } from '@angular/core';
 export class MultimediaService {
   callback: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() {}
+  public audio!: HTMLAudioElement;
+  public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
+
+  constructor() {
+    this.audio = new Audio();
+    this.trackInfo$.subscribe((resOk) => {
+      if (resOk) {
+        this.setAudio(resOk);
+      }
+    });
+  }
+
+  public setAudio(track: TrackModel) {
+    this.audio.src = track.url;
+    this.audio.play();
+  }
 }
