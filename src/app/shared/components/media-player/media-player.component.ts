c4b9a080a9e03dc743d7ad563ebe12c4fa2463e2
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-08 22:37:47 -0400

    Use multimedia service

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/services/multimedia.service';
 import { Subscription } from 'rxjs';
 
@@ -9,37 +8,11 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
-  mockCover: TrackModel = {
-    _id: 1,
-    cover:
-      'https://e-cdn-images.dzcdn.net/images/cover/9b6da786cd3ca8b286a04186b3c9079c/264x264-000000-80-0-0.jpg',
-    album: '?',
-    name: 'Moonlight',
-    artist: {
-      name: 'XXXTENTACION',
-      nickname: 'xxxtentacion',
-      nationality: 'US',
-    },
-    duration: {
-      start: 0,
-      end: 220,
-    },
-    url: 'http://localhost:4000/x.mp3',
-  };
-
   listObserver$: Array<Subscription> = [];
 
-  constructor(private _multimediaService: MultimediaService) {}
+  constructor(public _multimediaService: MultimediaService) {}
 
-  ngOnInit(): void {
-    const observer1$: Subscription = this._multimediaService.callback.subscribe(
-      (response: TrackModel) => {
-        console.log('Recibiendo canción', response);
-      }
-    );
-
-    this.listObserver$ = [observer1$];
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.listObserver$.forEach((suscription) => suscription.unsubscribe);
