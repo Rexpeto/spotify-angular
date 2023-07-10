d315e848d692d9f75ff5402f6f04210b19b84061
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-10 12:34:06 -0400

    Calculate px in progress bar and use service

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MultimediaService } from '@shared/services/multimedia.service';
 import { Subscription } from 'rxjs';
 
@@ -8,6 +14,8 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
+  @ViewChild('progressBar') progressBar: ElementRef = new ElementRef('');
+
   listObserver$: Array<Subscription> = [];
   state: string = 'paused';
 
@@ -23,5 +31,15 @@ export class MediaPlayerComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.listObserver$.forEach((suscription) => suscription.unsubscribe);
+  }
+
+  handlePosition(event: MouseEvent) {
+    const { clientX } = event;
+    const elNative: HTMLElement = this.progressBar.nativeElement;
+    const { x, width } = elNative.getBoundingClientRect();
+    const clickX = clientX - x;
+    const porcentage = (clickX * 100) / width;
+
+    this._multimediaService.seekAudio(porcentage);
   }
 }
