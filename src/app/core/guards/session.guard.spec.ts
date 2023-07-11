4402808ff877f91f7515e4bde35242cf33f0da2c
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-11 00:39:34 -0400

    Testing of session guard

--- src/app/core/guards/session.guard.spec.ts
+++ src/app/core/guards/session.guard.spec.ts
@@ -1,17 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { sessionGuard } from './session.guard';
+import { SessionGuard } from './session.guard';
 
-describe('sessionGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => sessionGuard(...guardParameters));
+describe('Testing of Session Guard 👍', () => {
+  let guard: SessionGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+    });
+    guard = TestBed.inject(SessionGuard);
   });
-
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
   });
 });
