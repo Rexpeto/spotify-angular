d602bd1e364f45c59e752d1804bcd39381ae23b5
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-11 17:08:56 -0400

    Testing filter in list

--- src/app/shared/pipe/order-list.pipe.spec.ts
+++ src/app/shared/pipe/order-list.pipe.spec.ts
@@ -1,8 +1,23 @@
 import { OrderListPipe } from './order-list.pipe';
+import * as mockRaw from '../../data/track.json';
+import { TrackModel } from '@core/models/tracks.model';
 
 describe('OrderListPipe', () => {
   it('create an instance', () => {
     const pipe = new OrderListPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  //* Input and output values
+  it('Input and output values', () => {
+    //? Arrage
+    const pipe = new OrderListPipe();
+    const { data }: any = mockRaw;
+
+    //? Act
+    const result: TrackModel[] = pipe.transform(data, 'name', 'asc');
+
+    //? Assert
+    expect(result).toEqual(data);
   });
 });
