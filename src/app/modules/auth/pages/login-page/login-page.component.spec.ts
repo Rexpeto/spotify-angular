aafa13035815b8edb15b5751965dba4dcba341b6
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-11 00:41:11 -0400

    Testing initial of auth login page

--- src/app/modules/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginPageComponent } from './login-page.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -8,7 +11,8 @@ describe('LoginPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginPageComponent]
+      declarations: [LoginPageComponent, SideBarComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
     });
     fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
