513b70bc8776574937d47b99bb180f941ff984ec
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-11 00:45:39 -0400

    Testing initial of history page

--- src/app/modules/history/pages/history-page/history-page.component.spec.ts
+++ src/app/modules/history/pages/history-page/history-page.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HistoryPageComponent } from './history-page.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
+import { SearchComponent } from '@modules/history/components/search/search.component';
+import { FormsModule } from '@angular/forms';
+import { PlayListBodyComponent } from '@shared/components/play-list-body/play-list-body.component';
+import { OrderListPipe } from '@shared/pipe/order-list.pipe';
 
 describe('HistoryPageComponent', () => {
   let component: HistoryPageComponent;
@@ -8,7 +14,14 @@ describe('HistoryPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HistoryPageComponent]
+      declarations: [
+        HistoryPageComponent,
+        HomePageComponent,
+        SearchComponent,
+        PlayListBodyComponent,
+        OrderListPipe,
+      ],
+      imports: [HttpClientTestingModule, FormsModule],
     });
     fixture = TestBed.createComponent(HistoryPageComponent);
     component = fixture.componentInstance;
